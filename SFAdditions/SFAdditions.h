5e044c72026d79e9e97cee8788c536c4931a2230
Skye <gskye@aol.com>
2015-08-04 13:57:38 -0400

    Initial Commit

---
+++ SFAdditions/SFAdditions.h
@@ -0,0 +1,13 @@
+//
+//  SFAdditions.h
+//  SFAdditions
+//
+//  Created by Skye on 8/4/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFAdditions : NSObject
+
+@end
---
+++ SFAdditions/SFAdditions.m
@@ -0,0 +1,13 @@
+//
+//  SFAdditions.m
+//  SFAdditions
+//
+//  Created by Skye on 8/4/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import "SFAdditions.h"
+
+@implementation SFAdditions
+
+@end
